dd10ae4a7092e56918419aa9696825487344b6ca
Michael Wales <mwales3@gmail.com>
2017-06-28 00:02:46 -0400

    Updated my hashsort program for some fslint like tasks

--- hashsort/hashsort.cpp
+++ hashsort/hashsort.cpp
@@ -3,12 +3,15 @@
 #include <algorithm>
 #include <exception>
 #include <string.h>
+#include <map>
 
 void printUsage()
 {
    std::cerr << "Usage: hashsort [-f] [-h] < hashes_to_sort.md5" << std::endl;
    std::cerr << "  -f : sort hashes by filename" << std::endl;
    std::cerr << "  -h : sort hashes by hash" << std::endl;
+   std::cerr << "  -n : print out non-uniques (will sort by hash too)" << std::endl;
+   std::cerr << "  -u : print out uniques only (will sort by hash too)" << std::endl;
    std::cerr << std::endl;
    std::cerr << "This application intended for sorting files created by md5sum.  It reads the hash file on stdin" << std::endl;
    std::cerr << "and writes the output on stdout" << std::endl;
@@ -30,6 +33,59 @@ bool sortByFilename(std::string const & a, std::string const & b)
    return (a.substr(endOfHashA + 1) < b.substr(endOfHashB + 1));
 }
 
+void performMultiMapSort(std::vector<std::string> const & lines, std::string sortType)
+{
+   bool uniquesOnly = false;
+   if (sortType == "-u")
+   {
+      uniquesOnly = true;
+   }
+   else
+   {
+      uniquesOnly = false;
+   }
+
+   std::multimap<std::string, std::string> bigMap;
+   for(auto singleLine : lines)
+   {
+      size_t endOfHash = singleLine.find(' ');
+      bigMap.insert(std::make_pair(singleLine.substr(0, endOfHash), singleLine.substr(endOfHash + 1)));
+   }
+
+   std::string lastHash = "";
+   for(auto record : bigMap)
+   {
+      std::string thisHash = record.first;
+
+      if (bigMap.count(thisHash) == 1)
+      {
+         // This hash is unique
+         if (uniquesOnly)
+         {
+            std::cout << thisHash << "\t" << record.second << std::endl;
+         }
+      }
+      else
+      {
+         if (!uniquesOnly)
+         {
+            // This hash isn't unique
+            if (thisHash == lastHash)
+            {
+               // Print it as a dupe
+               std::cout << "DUPE\t" << thisHash << "\t" << record.second << std::endl;
+            }
+            else
+            {
+               std::cout << "UNIQ\t" << thisHash << "\t" << record.second << std::endl;
+            }
+
+            lastHash = thisHash;
+         }
+      }
+   }
+}
+
 int main(int argc, char* argv[])
 {
    if (argc != 2)
@@ -39,6 +95,7 @@ int main(int argc, char* argv[])
    }
 
    bool useFilenameSort = true;
+   bool useMultiMapSort = false;
 
    if (strcmp("-f", argv[1]) == 0)
    {
@@ -47,6 +104,14 @@ int main(int argc, char* argv[])
    else if (strcmp("-h", argv[1]) == 0)
    {
       useFilenameSort = false;
+   }
+   else if (strcmp("-n", argv[1]) == 0)
+   {
+      useMultiMapSort = true;
+   }
+   else if (strcmp("-u", argv[1]) == 0)
+   {
+      useMultiMapSort = true;
    }
    else
    {
@@ -62,7 +127,13 @@ int main(int argc, char* argv[])
       linesOfText.push_back(singleLine);
 	}
 
-   if (useFilenameSort)
+   if (useMultiMapSort)
+   {
+      performMultiMapSort(linesOfText, argv[1]);
+      return 0;
+   }
+
+   if (!useFilenameSort)
    {
       // My c++ lambda viriginity was taken below...
       std::sort(linesOfText.begin(), linesOfText.end(),
