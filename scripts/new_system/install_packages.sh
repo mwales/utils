e13f39e822f28aa8227f2ce61c45c88c5d231722
Michael Wales <mwales3@gmail.com>
2020-01-29 01:08:00 -0500

    Created script to install packages I want on fresh Ubuntu system

---
+++ scripts/new_system/install_packages.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt-get install android-sdk-platform-tools-common android-tools-adb arandr asciidoc audacity avidemux-qt -y
+sudo apt-get install binutils-dev binwalk blender build-essential calibre ckermit clang cmake cpio curl -y
+sudo apt-get install dolphin-emu dos2unix dosbox doxygen-gui dvdauthor easytag exfat-fuse exfat-utils -y
+sudo apt-get install fakeroot fceux fdisk ffmpeg flac ftp fslint -y
+sudo apt-get install g++ gcc-arm-linux-gnueabi gdb-multiarch gdbserver geany gdisk gedit gens gftp ghex gimp git gitk gnuradio gparted graphviz guake gwenview -y
+sudo apt-get install handbrake hdparm hexchat higan htop hugin -y
+sudo apt-get install i2c-tools ifupdown imagemagick inkscape ioquake3 iotop ipython jstest-gtk -y
+sudo apt-get install k4dirstat k3b kdenlive kompare -y
+sudo apt-get install lynx libreoffice-common mame meld mplayer nano nasm netcat-traditional netcat-openbsd net-tools nmap okular openscad openshot openssh-server -y
+sudo apt-get install photoprint p7zip putty oracle-java8-installer pavucontrol pcsxr picocom plexmediaserver pyqt5-dev -y
+sudo apt-get install qdirstat qemu-system qemu-user qemu-utils qt5-doc qt5-qmake qtbase5-dev-tools qtcreator -y
+sudo apt-get install rar remmina rsync smplayer socat sound-juicer squashfs-tools steam supertuxkart synaptic -y
+sudo apt-get install telnet tmux transmission umbrello unetbootin usb-creator-gtk vice vim valgrind virtualbox-qt vlc vokoscreen -y
+sudo apt-get install wget wine-stable winetricks wireshark youtube-dl zsnes yabause -y
