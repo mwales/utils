b641281f15c5b30646a75fe0e4dc5bf1d7aea561
Michael Wales <mwales3@gmail.com>
2020-03-23 01:55:43 -0400

    Update to my new system script

--- scripts/new_system/install_packages.sh
+++ scripts/new_system/install_packages.sh
@@ -8,12 +8,14 @@ sudo apt-get install g++ gcc-arm-linux-gnueabi gdb-multiarch gdbserver geany gdi
 sudo apt-get install handbrake hdparm hexchat higan htop hugin -y
 sudo apt-get install i2c-tools ifupdown imagemagick inkscape ioquake3 iotop ipython jstest-gtk -y
 sudo apt-get install k4dirstat k3b kdenlive kompare -y
-sudo apt-get install lynx libreoffice-common mame meld mplayer nano nasm netcat-traditional netcat-openbsd net-tools nmap okular openscad openshot openssh-server -y
+sudo apt-get install lynx libreoffice-common mame meld mplayer -y
+sudo apt-get install nano nasm netcat-traditional netcat-openbsd net-tools nfs-common -y
+sudo apt-get install nmap okular openscad openshot openssh-server -y
 sudo apt-get install p7zip putty pavucontrol pcsxr picocom pyqt5-dev -y
-sudo apt-get install qdirstat qemu-system qemu-user qemu-utils qt5-doc qt5-qmake qtbase5-dev-tools qtcreator -y
-sudo apt-get install rar remmina rsync smplayer socat sqlitebrowser sound-juicer squashfs-tools steam supertuxkart synaptic -y
+sudo apt-get install qdirstat qemu-system qemu-user qemu-utils qt5-default qt5-doc qt5-qmake qtbase5-dev-tools qtcreator -y
+sudo apt-get install rar remmina rsync smartmon-tools smplayer socat sqlitebrowser sound-juicer squashfs-tools steam supertuxkart synaptic -y
 sudo apt-get install telnet tmux transmission umbrello usb-creator-gtk vice vim valgrind virtualbox-qt vlc vokoscreen -y
-sudo apt-get install wget wine-stable winetricks wireshark youtube-dl zsnes yabause -y
+sudo apt-get install wget wine-stable winetricks wireshark xfce4-sensors-plugin youtube-dl zsnes yabause -y
 
 
 # Things that used to be in there that aren't anymore
